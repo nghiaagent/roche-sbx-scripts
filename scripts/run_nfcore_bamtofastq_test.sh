7ec1f42dd378aa9356ce90f92aa2baad0e4ffac3
nghiaagent <nghia6a1@gmail.com>
2026-06-26 14:18:10 +1000

    Make script run well when only /scratch is available
    and switch to Roche data

--- scripts/run_nfcore_bamtofastq_test.sh
+++ scripts/run_nfcore_bamtofastq_test.sh
@@ -1,13 +1,14 @@
 # DO NOT SUBMIT THIS SCRIPT VIA SBATCH
 # Log in to setonix-workflow then run it interactively.
 
+# Load modules
+module load nextflow/25.04.6
+module load singularity/4.1.0-slurm
+
 # Export appropriate environment variables
-export NXF_HOME="$MYSCRATCH/.nextflow"
-export NXF_SINGULARITY_CACHEDIR="$MYSCRATCH/.nextflow_singularity"
-export SINGULARITY_CACHEDIR="$MYSCRATCH/.singularity"
+export NXF_HOME="${MYSCRATCH}/.nextflow"
+export NXF_SINGULARITY_CACHEDIR="${MYSCRATCH}/.nextflow_singularity"
+export SINGULARITY_CACHEDIR="${MYSCRATCH}/.singularity"
 
 # Execute Nextflow job
-nextflow run nf-core/bamtofastq -r 2.0.0\ 
-    -profile singularity,pawsey_setonix \
-    --input data/sample_sheet_bamtofastq.csv
-    --outdir $MYSCRATCH/results_bamtofastq
+nextflow run nf-core/bamtofastq -r 2.0.0 -profile singularity,pawsey_setonix --input "${PWD}/data/sample_sheet_bamtofastq.csv" --outdir "${MYSCRATCH}/results_bamtofastq"
