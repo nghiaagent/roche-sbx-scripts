e3dfda5f7750bfafc617588eaf528cbbd9ee7d18
Nghia Nguyen <nghia6a1@gmail.com>
2026-06-27 10:45:04 +1000

    Add tests, prep for seqinspector

--- scripts/make_samplesheets.R
+++ scripts/make_samplesheets.R
@@ -16,34 +16,48 @@ username <- "nghiaagent"
 
 # Define samples that are in use at the moment
 samples <- c(
-  "50918036C_HPSID_002",
-  "50918124C_HPSID_004",
-  "50918402C_HPSID_005",
-  "50918538C_HPSID_006",
-  "50918870C_HPSID_007"
+  "50918036C_HPSID_002_L1",
+  "50918124C_HPSID_004_L1",
+  "50918402C_HPSID_005_L1",
+  "50918538C_HPSID_006_L1",
+  "50918870C_HPSID_007_L1"
 )
 
-# Calculate paths to where files should be ($MYSCRATCH/roche)
-paths_bam <- str_c(
+folders <- str_remove(samples, "_L1")
+
+# Check that all samples are present
+sample_sheet <- read_csv(here("data/sample_sheet.csv"))
+
+if (!all(samples %in% sample_sheet$sample_name)) {
+  stop("One or more samples are not present in provided sample sheet")
+}
+
+# Calculate paths to where files should be ($MYSCRATCH/roche and $MYSCRATCH/roche_fastq)
+paths_cram <- str_c(
   "/scratch/",
   project,
   "/",
   username,
   "/roche/",
-  samples,
+  folders,
   "/",
   samples,
-  "_L1.cram",
+  ".cram",
   sep = ""
 )
+
+#### TODO: FastQ paths
 
 # Build nf-core/bamtofastq sample sheet
 # Columns specs: sample_id, mapped, index, filetype
 sample_sheet_bamtofastq <- data.frame(
   sample_id = samples,
-  mapped = paths_bam
+  mapped = paths_cram
 ) |>
   mutate(index = NA, file_type = "cram")
+
+# Build nf-core/seqinspector sample sheet
+# TODO!!!
 
 # Export files
 ## bamtofastq
