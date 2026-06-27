aa2c02d1c62eb186cfbdad1a8615cf778237cf0b
Nghia Nguyen <nghia6a1@gmail.com>
2026-06-27 18:13:12 +1000

    disable samtools collate fast

--- scripts/run_nfcore_bamtofastq.sh
+++ scripts/run_nfcore_bamtofastq.sh
@@ -11,4 +11,4 @@ export NXF_SINGULARITY_CACHEDIR="${MYSCRATCH}/.nextflow_singularity"
 export SINGULARITY_CACHEDIR="${MYSCRATCH}/.singularity"
 
 # Execute Nextflow job
-nextflow run nf-core/bamtofastq -r 2.0.0 -profile singularity,pawsey_setonix -c ./nextflow.config --samtools_collate_fast --input "${PWD}/data/sample_sheet_bamtofastq.csv" --outdir "${MYSCRATCH}/roche_fastq"
+nextflow run nf-core/bamtofastq -r 2.0.0 -profile singularity,pawsey_setonix -c ./nextflow.config --input "${PWD}/data/sample_sheet_bamtofastq.csv" --outdir "${MYSCRATCH}/roche_fastq"
