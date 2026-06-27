fb88607dbe11173c395a7bc23e6652873e6caa61
Nghia Nguyen <nghia6a1@gmail.com>
2026-06-27 10:20:35 +1000

    First commit

---
+++ scripts/make_samplesheets.R
@@ -0,0 +1,9 @@
+######################
+# In this script, I create my sample sheets automatically
+# For nf-core/bamtofastq
+# And nf-core/seqinspector
+######################
+
+# Declare location
+
+# Import packages
