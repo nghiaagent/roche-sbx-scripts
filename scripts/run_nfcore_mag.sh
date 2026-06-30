a58aeae03661b17156d5514f2c6a27ed400a4b2a
nghiaagent <nghia6a1@gmail.com>
2026-06-30 16:47:35 +1000

    Add more parameters for nf-core/mag

--- scripts/run_nfcore_mag.sh
+++ scripts/run_nfcore_mag.sh
@@ -12,4 +12,4 @@ export NXF_SINGULARITY_CACHEDIR="${MYSCRATCH}/.nextflow_singularity"
 export SINGULARITY_CACHEDIR="${MYSCRATCH}/.singularity"
 
 # Execute Nextflow job
-./nextflow run nf-core/mag -r 5.4.2 -profile singularity,pawsey_setonix -c ./nextflow.config --input "${PWD}/data/sample_sheet_mag.csv" --outdir "${MYSCRATCH}/roche_mag" --single_end --host_genome "Homo_sapiens_NCBI_GRCh38"
+./nextflow run nf-core/mag -r 5.4.2 -profile singularity,pawsey_setonix -resume -c ./nextflow.config -params-file params_mag.yaml --input "${PWD}/data/sample_sheet_mag.csv" --outdir "${MYSCRATCH}/roche_mag" 
